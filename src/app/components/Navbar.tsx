b0ccedbb024b17fb657ae7885889a834032760ad
João Vitor <joaovitorvianaalves@gmail.com>
2024-03-25 15:43:34 -0300

    .

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,13 +8,7 @@ export default function Navbar() {
   const path = usePathname();
 
   return (
-    <div
-      className={
-        window.innerWidth > 768
-          ? "flex justify-between bg-[#2F2878] p-4"
-          : "flex justify-between bg-[#2F2878] p-4"
-      }
-    >
+    <div className={"flex justify-between bg-[#2F2878] p-4"}>
       <Image className="mx-4" src={"/g1.png"} alt="" width={150} height={1} />
       <ul className="sm:flex gap-8 text-white text-lg items-center px-16">
         <li>
