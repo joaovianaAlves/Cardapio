d6afb79aa169e42264e2e80536cb61638c3f4ffa
João Vitor <joaovitorvianaalves@gmail.com>
2024-04-23 17:52:26 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import CardMeta from "./components/CarMeta";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center text-4xl p-6 py-7 font-semibold">Meta Apps</h1>
+      <h1 className="text-center text-4xl p-6 py-7 font-semibold">
+        Programas Nao
+      </h1>
       <CardNao />
     </div>
   );
